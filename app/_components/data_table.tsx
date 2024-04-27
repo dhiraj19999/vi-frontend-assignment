2bb4fb303d3353c9574ef2981627a727fa43569d
Dhiraj Garad <yogeshsamse682@gmail.com>
2024-04-27 16:02:06 +0530

    Update data_table.tsx

--- app/_components/data_table.tsx
+++ app/_components/data_table.tsx
@@ -16,6 +16,7 @@ import {
     useReactTable,
 } from "@tanstack/react-table";
 import { DataTablePagination } from "./data-table-pagination";
+import { useState } from "react";
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
@@ -23,11 +24,13 @@ interface DataTableProps<TData, TValue> {
 }
 
 export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+    const [selectedRow, setSelectedRow] = useState(null);
     const table = useReactTable({
         data,
         columns,
         getCoreRowModel: getCoreRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
+        columnResizeMode: "onChange"
     });
 
     // TASK : Make first 2 columns (i.e. checkbox and task id) sticky
@@ -36,19 +39,21 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
     return (
         <div className="space-y-4">
             <div className="rounded-md border">
-                <Table>
+                <Table style={{minWidth: table.getTotalSize()}}>
                     <TableHeader>
                         {table.getHeaderGroups().map((headerGroup) => (
                             <TableRow key={headerGroup.id}>
                                 {headerGroup.headers.map((header) => {
                                     return (
-                                        <TableHead key={header.id} colSpan={header.colSpan}>
+                                        <TableHead key={header.id} colSpan={header.colSpan} className={header.id === "checkbox" ? "sticky left-0 bg-white" : "" || header.id === "id" ? "sticky left-[88px] bg-white" : ""}
+                                            style={{ width: header.getSize() }}>
                                             {header.isPlaceholder
                                                 ? null
                                                 : flexRender(
-                                                      header.column.columnDef.header,
-                                                      header.getContext(),
-                                                  )}
+                                                    header.column.columnDef.header,
+                                                    header.getContext(),
+                                                )}
+                                            <div className="bg-black w-1 h-6 float-right select-none touch-none cursor-col-resize" onMouseDown={header.getResizeHandler()} onTouchStart={header.getResizeHandler()}></div>
                                         </TableHead>
                                     );
                                 })}
@@ -63,7 +68,11 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
                                     data-state={row.getIsSelected() && "selected"}
                                 >
                                     {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
+                                        <TableCell key={cell.id} onClick={cell.column.id === "checkbox" ? () => {
+                                            table.resetRowSelection(); row.toggleSelected();
+                                        } : () => { }}
+                                            className={`${!row.getIsSelected() && "bg-white"} ${row.getIsSelected() && "bg-gray-300"} ${cell.column.id === "checkbox" && "sticky left-0"} ${cell.column.id === "id" && "sticky left-[88px]"}`}
+                                            style={{ width: cell.column.getSize() }}>
                                             {flexRender(
                                                 cell.column.columnDef.cell,
                                                 cell.getContext(),
@@ -83,6 +92,6 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
                 </Table>
             </div>
             <DataTablePagination table={table} />
-        </div>
+        </div >
     );
 }
